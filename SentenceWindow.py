9232f305c9581b77193752dfed8025f6db148a0f
yoav.meron <yoav.meron11@gmail.com>
2025-02-04 12:03:46 +0200

    Final Commit

--- SentenceWindow.py
+++ SentenceWindow.py
@@ -1,11 +1,12 @@
 class SentenceWindow:
     def __init__(self, sentences):
+        # List of sentences
         self.sentences = sentences
 
     # Function iterates over sentences and finds if person exists in window
     def find_person_in_window(self, person):
         for sentence in self.sentences:
-            if sentence.check_if_person_in_sentence(person):
+            if sentence.check_if_person_in_sentence(person): # Check if person in sentence
                 return True
         return False
 
